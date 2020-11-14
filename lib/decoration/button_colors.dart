958faee7b956c8b1a7c328f53742ad0c3a87a5e8
Ilker Bulut <ilkerbulut1@msn.com>
2020-11-14 01:53:24 +0100

    minor changes

--- lib/decoration/button_colors.dart
+++ lib/decoration/button_colors.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
 class ButtonColors {
-  static get backgroundColor => const Color.fromARGB(86, 61, 124, 1);
+  static get appBarColor => const Color.fromRGBO(86, 61, 124, 1);
+  static get backgroundColor => const Color.fromRGBO(248, 249, 250, 1);
 }
